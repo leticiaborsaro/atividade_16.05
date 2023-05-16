09ffbd6422ea7893fc0c205d597a0f1423885e20
leticiaborsaro <leleborsaro@gmail.com>
2023-05-16 09:19:58 -0300

    questao 1

---
+++ script.js
@@ -0,0 +1,19 @@
+//atividade dia 16/05
+
+// Questão 1
+
+    var n1 = parseInt(window.prompt("1) Insira um número inteiro"));
+    var n2 = parseInt(window.prompt("Insira outro número inteiro:"));
+    var n3 = parseInt(window.prompt("Insira mais um número:"));
+
+   document.write('1) A média é ', Math.floor((n1 + n2 + n3)/3), '\n');
+
+// Questão 2
+
+   /* let nome = window.prompt('2) Digite seu nome:')
+    let sal = window.prompt('Digite o seu salário:')
+
+    let aumentosal = sal/10 
+    let novosal = aumentosal + sal
+
+    document.write('2) R= Seu novo salário é', novosal) */
